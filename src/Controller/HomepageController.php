49ed26a91e54f02b01891bac4801dd25b41dd205
Luigi213 <reyesgianluigi88@gmail.com>
2023-11-30 10:57:43 +0100

    step 1, 2, 3, 4 and 8 completed

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomepageController extends AbstractController
 {
     #[Route('/homepage', name: 'app_homepage')]
-    public function index(): Response
+    public function index(PostRepository $postRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $query = $postRepository->createQueryBuilder('i')
+            ->orderBy('i.id', 'DESC')
+            ->getQuery();
+
+        $posts = $paginator->paginate(
+            $query, // Query da paginare
+            $request->query->getInt('page', 1), // Parametro opzionale per il numero di pagina, predefinito a 1
+            10 // Numero di elementi per pagina
+        );
+
         return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+            'posts' => $posts,
         ]);
     }
 }
