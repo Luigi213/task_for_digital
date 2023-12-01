bb32fcad7b54702230cf36626272269657166614
Luigi213 <reyesgianluigi88@gmail.com>
2023-12-01 12:58:42 +0100

    optimized

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,9 +10,20 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $words = ['Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet', 'Consectetur', 'Adipiscing', 'Elit'];
+
         for ($i = 0; $i < 50; $i++) {
             $post = new Post();
-            $post->setTitle("Post " . $i);
+
+            // Genera un titolo casuale combinando alcune parole casuali
+            $title = '';
+            $numWords = rand(2, 5); // Numero casuale di parole nel titolo
+            for ($j = 0; $j < $numWords; $j++) {
+                $title .= $words[array_rand($words)] . ' ';
+            }
+
+            $post->setTitle(trim($title));
+
             $post->setImage("https://picsum.photos/200/300?random=" . $i);
 
             // Aggiunge un campo data con date casuali nei prossimi 50 giorni
